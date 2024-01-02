a8e66f81cfa7a8815e87498509eadff75190e07f
Pankaj Bhadane <pankajb2601@gmail.com>
2024-01-02 07:09:21 +0530

    added another method/button to close the mealinfo page

--- src/App.jsx
+++ src/App.jsx
@@ -15,22 +15,36 @@ function App() {
     const searchMeal = async () => {
       try {
         const response = await axios.get(
-          `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`
+          `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}&limit=5`
         );
         setResult(response.data.meals);
       } catch (error) {
         console.error("Error while fetching data: ", error);
       }
     };
-    setSelectedMeal(null)
+    setSelectedMeal(null);
     searchMeal();
   }, [query]);
+
+  // approach with buttton click
+  // const searchMeal = async () => {
+  //   try {
+  //     const response = await axios.get(
+  //       `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`
+  //     );
+  //     setResult(response.data.meals);
+  //   } catch (error) {
+  //     console.error("Error while fetching data: ", error);
+  //   }
+  //   setSelectedMeal(null);
+  // };
 
   const handleMealClick = (meal) => {
     console.log(meal);
     setSelectedMeal(meal);
   };
 
+  // style the app.
   return (
     <div>
       <h1>Meal Searcher</h1>
@@ -44,9 +58,14 @@ function App() {
       </div>
       <div>
         {selectedMeal ? (
-          <MealInfo selectedMeal={selectedMeal} />
+          <div>
+            <MealInfo selectedMeal={selectedMeal} />
+            <button onClick={() => setSelectedMeal(null)}>Close</button>
+          </div>
         ) : (
-          <MealMenu meals={result} onMealClick={handleMealClick} />
+          <div>
+            <MealMenu meals={result} onMealClick={handleMealClick} />
+          </div>
         )}
       </div>
     </div>
--- src/components/MealMenu.jsx
+++ src/components/MealMenu.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
 const MealMenu = ({ meals, onMealClick }) => {
+  const sliceMeals = meals.slice(0, 9);
   return (
     <div>
-      {meals && meals.length > 0 ? (
-        meals.map((meal) => (
+      {sliceMeals && sliceMeals.length > 0 ? (
+        sliceMeals.map((meal) => (
           <div key={meal.idMeal} onClick={() => onMealClick(meal)}>
             <h3>{meal.strMeal}</h3>
             <img src={meal.strMealThumb} alt={meal.strMeal} />
